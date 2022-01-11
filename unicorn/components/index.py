8266816c73a304942e93de22cf05db550cf30e55
Putra Dwiyanto <putra.dwiyanto@photoninfotech.net>
2022-01-11 15:43:14 +0700

    update views

--- unicorn/components/index.py
+++ unicorn/components/index.py
@@ -2,7 +2,7 @@ from django_unicorn.components import UnicornView
 from unicorn.components.moviePreview import MoviepreviewView
 
 class IndexView(UnicornView):
-    
+    data= {}
     def onTitleClick(self, title="", desc="", poster=""):
         self.data["selected"]["title"] = title
         self.data["selected"]["desc"] = desc
